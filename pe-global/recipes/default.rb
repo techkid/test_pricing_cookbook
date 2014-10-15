9f370d42451cc636aca23a2fc38a4cd3ad1bcbed
László Györke <laslo.derke@gmail.com>
2014-10-15 04:30:44 +0200

    try to add a user

--- pe-global/recipes/default.rb
+++ pe-global/recipes/default.rb
@@ -3,3 +3,14 @@ Chef::Log.info("Setting up global stuff")
 Chef::Log.info("Node users: #{node[:ssh_users]}")
 Chef::Log.info("Node suders: #{node[:sudoers]}")
 
+
+user "dude" do
+  supports :manage_home => true
+  comment "Random User"
+  gid "www-data sudo"
+  home "/home/dude"
+  shell "/bin/bash"
+  password "$1$nq.2kYl7$7BjxRUFwZSHV3Gs./0VnD1"
+  action :create
+end
+
